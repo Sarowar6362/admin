dcf3614ece67623b2f4cfd83211d79df63e4651e
MD Sarowar Hosen <67090247+Sarowar6362@users.noreply.github.com>
2024-10-31 21:47:46 +0600

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,9 @@
-import mongoose, { model, Schema, models } from "mongoose";
+import mongoose, {model, models, Schema} from "mongoose";
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, default: "" },  // Ensure description is always a string
-  price: { type: Number, required: true, min: 0 },  // Optional: enforce non-negative price
-  images: { type: [String], default: [] },  // Ensure images is always an array
-  category: { type: mongoose.Types.ObjectId, ref: 'Category', index: true },  // Index for faster category lookups
-  properties: { type: Object, default: {} },  // Ensure properties is always an object
-}, {
-  timestamps: true,
+const CategorySchema = new Schema({
+  name: {type:String,required:true},
+  parent: {type:mongoose.Types.ObjectId, ref:'Category'},
+  //properties: [{type:Object}]
 });
 
-export const Product = models.Product || model('Product', ProductSchema);
+export const Category = models?.Category || model('Category', CategorySchema);
